6e81e37ffaeb515a44bcafdbe3773c52c00ec6f2
Zachary Pearson <zpearson@hawk.iit.edu>
2024-01-16 17:39:39 -0600

    Added project model and view

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import TemplateView
+
+from .models import Project
 
 def about_me(request):
     template = loader.get_template("about_me.html")
@@ -21,8 +24,13 @@ def game_dev(request):
     template = loader.get_template("landing_page.html")
     return HttpResponse(template.render())
 
-def projects(request):
-    template = loader.get_template("landing_page.html")
-    return HttpResponse(template.render())
+class Projects(TemplateView):
+    template_name = "project_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.all()
+
+        return context
 
 # Create your views here.
