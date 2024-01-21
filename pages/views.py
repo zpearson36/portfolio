c60a261e58c3ee60aede97ab87665b32fa206428
Zachary Pearson <zpearson@hawk.iit.edu>
2024-01-21 16:39:11 -0600

    Added Contact Me view and template

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, FormView, TemplateView
 
 from .models import Project, MyInfo
 
@@ -42,4 +42,13 @@ class ProjectDetail(DetailView):
     template_name = "project_detail.html"
     model = Project
     context_object_name = "project"
+
+class ContactMe(TemplateView):
+    template_name = "contact_me.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_info'] = MyInfo.objects.all()[0]
+
+        return context
 # Create your views here.
